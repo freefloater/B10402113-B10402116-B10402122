af6d770f54f169175395d7daaf0f5d4fa638ee38
stevelee1314 <33798235+stevelee1314@users.noreply.github.com>
2017-11-19 16:27:31 +0800

    problem3.

--- B10402113B10402116B10402122/B10402113B10402116B10402122/ClassB.cs
+++ B10402113B10402116B10402122/B10402113B10402116B10402122/ClassB.cs
@@ -8,5 +8,21 @@ namespace B10402113B10402116B10402122
 {
     class ClassB
     {
+         int j = 0; 
+for (int i = 2; i <= 1000; i++) 
+{ 
+if (i == 2 || i == 3 || i == 5 || i == 7) 
+{ 
+j++; 
+Console.Write(" " + i); 
+} 
+if (i % 2 != 0 && i % 3 != 0 && i % 5 != 0 && i % 7 != 0) 
+{ 
+j++; 
+Console.Write(" " + i); 
+if (j % 5 == 0) 
+Console.WriteLine(); 
+} 
+}
     }
 }
