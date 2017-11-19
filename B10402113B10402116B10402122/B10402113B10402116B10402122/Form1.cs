3714009b7abee8c96d63eebfc58a8a95835b8740
freefloater <floatermail@gmail.com>
2017-11-19 16:09:24 +0800

    problem1

---
+++ B10402113B10402116B10402122/B10402113B10402116B10402122/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace B10402113B10402116B10402122
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
