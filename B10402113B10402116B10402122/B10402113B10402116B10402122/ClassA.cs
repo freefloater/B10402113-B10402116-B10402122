12c4c171fc15c20c2e5384a1b8011c134d45bf91
z3388411 <33798111+z3388411@users.noreply.github.com>
2017-11-19 16:27:01 +0800

    problem2

--- B10402113B10402116B10402122/B10402113B10402116B10402122/ClassA.cs
+++ B10402113B10402116B10402122/B10402113B10402116B10402122/ClassA.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 namespace B10402113B10402116B10402122
 {
     class ClassA
-    {
+    {   
+          Random = new Random();//亂數種子
+          int i = Next(0, 100);   
     }
 }
